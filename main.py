c46762ad61e30d2a79b31cf3be71fc961022e9ce
DoxakisCh <57076806+DoxakisCh@users.noreply.github.com>
2020-01-04 21:59:02 +0200

    [ADD] collect features, plot confusion matrix function and new way to give features to the model

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import naive_bayes, metrics, model_selection
+from sklearn import naive_bayes, metrics, model_selection, neural_network
 from sklearn.pipeline import make_pipeline
 import utilities
+import numpy as np
 from time import perf_counter
 t1_start = perf_counter()
 
@@ -22,12 +23,14 @@ X, y = utilities.getData('\\resources\\Fake News Dataset.xlsx')
 
 
 # ==================== Phase 2 ======================
+# Collect Features from dataset
+features_text = utilities.collectFeatures(X)
 
 # Lemmatizing data
-lemmatized_doc = utilities.getLemmatizedText(X)
+lemmatized_text = utilities.getLemmatizedText(features_text)
 
 # Removing punctuation
-punct_removed_text = utilities.removePunctuation(lemmatized_doc)
+punct_removed_text = utilities.removePunctuation(lemmatized_text)
 
 # Lowercase text
 lowercase_text = utilities.lowercase(punct_removed_text)
@@ -36,7 +39,7 @@ lowercase_text = utilities.lowercase(punct_removed_text)
 stopword_removed_text = utilities.removeStopwords(lowercase_text)
 
 # Deleting unnecessary columns. Keeping only the preprocessed data
-list_columns = ["Title_stop", "Body_stop"]
+list_columns = ["Title_stop", "Body_stop", "Sum"]
 X = stopword_removed_text[list_columns]
 X = X.rename(columns={"Title_stop": "Title_Parsed", "Body_stop": "Body_Parsed"})
 X.to_excel("C:/Users/doxak/PycharmProjects/data.xls")
@@ -52,14 +55,31 @@ vectorizer = TfidfVectorizer(encoding='unicode', strip_accents='unicode', max_df
 
 # ==================== Phase 5 ======================
 # TODO:  NB doesn't seem to handle class imbalance well. We should try something else
-
+'''
 # Splitting the data
 x_train, x_test, y_train, y_test = model_selection.train_test_split(X['Body_Parsed'], y, random_state=42, stratify=y)
 alpha = 0.1
 model = make_pipeline(vectorizer, naive_bayes.MultinomialNB(alpha=alpha))
 model.fit(x_train, y_train)
 y_predicted = model.predict(x_test)
+'''
+# Perform TF-IDF in title and body and merge them with the other features
+vectorbody = vectorizer.fit_transform(X['Body_Parsed'])
+vector_body = vectorbody.toarray()
+vectortitle = vectorizer.fit_transform(X['Title_Parsed'])
+vector_title = vectortitle.toarray()
+vector_tfidf = np.concatenate((vector_body, vector_title), axis=1)
+features = X.as_matrix(columns=['Sum'])
+full_features = np.concatenate((vector_tfidf, features), axis=1)
 
+# Splitting the data
+x_train, x_test, y_train, y_test = model_selection.train_test_split(full_features, y, random_state=42, stratify=y)
+alpha = 0.1
+#model = naive_bayes.MultinomialNB(alpha=alpha)
+model = neural_network.MLPClassifier(hidden_layer_sizes=20, activation='relu', solver='adam', tol=0.0001, max_iter=100, alpha=alpha)
+
+model.fit(x_train, y_train)
+y_predicted = model.predict(x_test)
 
 # ==================== Phase 6 ======================
 
@@ -68,17 +88,21 @@ accuracy = metrics.accuracy_score(y_test, y_predicted)
 recall = metrics.recall_score(y_test, y_predicted, average='macro')
 precision = metrics.precision_score(y_test, y_predicted, average='macro')
 f1 = metrics.f1_score(y_test, y_predicted, average='macro')
+confusion_matrix = metrics.confusion_matrix(y_test, y_predicted)
 
 print("Accuracy: %f" % accuracy)
 print("Recall: %f" % recall)
 print("Precision: %f" % precision)
 print("F1: %f" % f1)
 
+# Plot confusion matrix
+utilities.plotHeatmap(confusion_matrix, alpha, accuracy, recall, precision, f1).show()
 
 # ==================== Phase 7 ======================
 # TODO:  What other features could we use?
 # Number of Entities mentioned?
 # Numbers of each POS appearances?
 
+# Calculate execution time
 t1_stop = perf_counter()
 print("Elapsed time:", t1_stop-t1_start)
--- utilities.py
+++ utilities.py
@@ -1,7 +1,9 @@
 import os
 import pandas as pd
 import spacy as sp
-from nltk.corpus import stopwords
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def getData(fileName):
@@ -13,6 +15,52 @@ def getData(fileName):
     X = data.iloc[:, :2]
     y = data.iloc[:, -1]
     return X, y
+
+def collectFeatures(df):
+    """
+       A function for collecting linguistic features
+    """
+
+    n = len(df)
+    # Collect the number of words
+    result1 = []
+    for i in range(0, n):
+        result1.append(len(re.findall(r'\S+', df['Title'][i])))
+    df['Number_of_words'] = result1
+
+    # Collect the number uppercase words bigger or equal to 5 letters
+    uppercase = []
+    for i in range(0, n):
+        result = re.findall(r'[Α-Ω]{5,}', df['Title'][i])
+        uppercase.append(len(result)/result1[i])
+    df['Uppercase_words'] = uppercase
+
+    # Collect the number of Ellispis(Αποσιωπητικά)
+    ellipsis =[]
+    for i in range(0, n):
+        result = re.findall(r'…', df['Title'][i])
+        ellipsis.append(len(result)/result1[i])
+    df['Ellipsis'] = ellipsis
+
+    # Collect the number of Guillemets(Εισαγωγικά)
+    guillements = []
+    for i in range(0, n):
+        result = re.findall(r'«.+?»', df['Title'][i])
+        guillements.append(len(result)/result1[i])
+    df['Guillements'] = guillements
+
+    # Collect the number of Exclamation Marks
+    exclamation = []
+    for i in range(0, n):
+        result = re.findall(r'!', df['Title'][i])
+        exclamation.append(len(result)/result1[i])
+    df['Exclamation Marks'] = exclamation
+
+    sum = []
+    for i in range(0, n):
+        sum.append(df['Exclamation Marks'][i] + df['Ellipsis'][i])
+    df['Sum'] = sum
+    return df
 
 
 def getLemmatizedText(df):
@@ -50,12 +98,18 @@ def getLemmatizedText(df):
 
 
 def removePunctuation(dataframe):
+    """
+       A function for removing punctuation
+    """
     df = dataframe
     df['Title_punct'] = df['Title_lem'].str.replace('[^\w\s]', '')
     df['Body_punct'] = df['Body_lem'].str.replace('[^\w\s]', '')
     return df
 
 def lowercase(dataframe):
+    """
+       A function for converting text to lowercase
+    """
     df = dataframe
     df['Title_lower'] = df['Title_punct'].str.lower()
     df['Body_lower'] = df['Body_punct'].str.lower()
@@ -67,7 +121,6 @@ def removeStopwords(lemmatized_text):
     """
     nlp = sp.load("el_core_news_sm")
     stop_words = list(nlp.Defaults.stop_words)
-    #stop_words1 = list(stopwords.words('greek'))
     df = lemmatized_text
     df['Title_stop'] = df['Title_lower']
     df['Body_stop'] = df['Body_lower']
@@ -79,3 +132,22 @@ def removeStopwords(lemmatized_text):
         df['Body_stop'] = df['Body_stop'].str.\
             replace(regex_stopword, '', case=False, regex=True)
     return df
+
+
+def plotHeatmap(confusion_matrix, alpha, accuracy, recall, precision, f1):
+    """
+    A function for plotting the confusion matrix
+    """
+    class_names = ['fake', 'legit']
+    df_cm = pd.DataFrame(confusion_matrix, index=class_names, columns=class_names)
+    fig = plt.figure(figsize=(10, 7))
+    heatmap = sns.heatmap(df_cm, annot=True, cmap='Blues', fmt="d")
+    heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
+    heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=45, ha='right', fontsize=14)
+    bottom, top = heatmap.get_ylim()
+    heatmap.set_ylim(bottom + 0.5, top - 0.5)
+    plt.title('Confusion Matrix \n[a={:.1f}] [Accuracy:{:.3f}, Recall:{:.3f}, Precision:{:.3f}, F1:{:.3f}]'
+              .format(alpha, accuracy, recall, precision, f1), fontsize=14)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    return fig
